e7d5da5ee09807d678ab88771878b50c18489a84
datuwu <doixuihen@gmail.com>
2022-05-19 10:06:56 +0700

    Update IProduct

--- PRN_SE1629_PRODUCT_MANAGEMENT/Manager/iProduct.cs
+++ PRN_SE1629_PRODUCT_MANAGEMENT/Manager/iProduct.cs
@@ -1,1 +1,11 @@
 ﻿using Prn.Se1629;
+
+public interface IProduct
+{
+    public abstract void Add(Product p);
+    bool Update(Product p);
+    bool Delete(Product p);
+    Product GetProduct(int productId);
+    void Display();
+
+}
--- PRN_SE1629_PRODUCT_MANAGEMENT/Program.cs
+++ PRN_SE1629_PRODUCT_MANAGEMENT/Program.cs
@@ -1,11 +1,2 @@
 ﻿namespace Prn.Se1629;
 
-public interface IProduct
-{
-    public abstract void Add(Product p);
-    bool Update(Product p);
-    bool Delete(Product p);
-    Product GetProduct(int productId);
-    void Display();
-
-}
