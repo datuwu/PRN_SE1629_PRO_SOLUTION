087ef6ece19d54ff3a621cf2b9c88712ba01258a
datuwu <doixuihen@gmail.com>
2022-05-31 10:39:31 +0700

    learn linq

---
+++ PRN_SE1629_LINQ/Student.cs
@@ -0,0 +1,23 @@
+﻿namespace PRN.Se1629;
+
+public class Student//:IComparable<Student>
+{
+    public int Id { get; set; }
+    public double Mark { get; set; }
+
+    public Student(int id, double mark)
+    {
+        Id = id;
+        Mark = mark;
+    }
+
+    public override string? ToString()
+    {
+        return $"Id = {Id}, Mark = {Mark}";
+    }
+    /*
+    public int CompareTo(Student? other)
+    {
+        return this.Mark.CompareTo(other.Mark);
+    }*/
+}
