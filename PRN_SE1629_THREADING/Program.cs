400061d3a0dbeca41d0a6bc1cff2377b02feef3f
datuwu <doixuihen@gmail.com>
2022-06-02 09:38:39 +0700

    learn threading

---
+++ PRN_SE1629_THREADING/Program.cs
@@ -0,0 +1,37 @@
+﻿using static System.Console;
+namespace Prn.Se1629;
+
+public class Program
+{
+    public static void Main()
+    {
+        //dong bo
+        /*
+        PrintNumber();
+        WriteLine("=============================");
+        PrintNumber();
+        WriteLine("=============================");
+        PrintNumber();
+        */
+
+        //Tao ra 3 threads, moi thread thuc thu loi goi PrintNumber
+        
+        for (int i = 1; i <= 5; i++)
+        {
+            Thread t = new Thread(() => 
+                PrintNumber(i)
+            );
+            t.Start();
+        }
+        
+    }
+
+    public static void PrintNumber(int thread)
+    {
+        for (int i = 1; i <=5; i++)
+        {
+            Thread.Sleep(1000);
+            WriteLine($"Thread: {thread} - {i}");
+        }
+    }
+}
