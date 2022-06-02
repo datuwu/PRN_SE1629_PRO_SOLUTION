80bea64abdfdbc02bb10495aba7d32858ac293e0
datuwu <doixuihen@gmail.com>
2022-06-02 11:06:17 +0700

    learn assemblies

---
+++ MathLib/MathLib.cs
@@ -0,0 +1,9 @@
+﻿namespace MathLib;
+
+public static class MathLib
+{
+    public static int Add(this int a, int b) => a + b;
+    public static int Sub(this int a, int b) => a - b;
+    public static void Display(this int data) 
+        => Console.WriteLine($"Calculation is: {data}");
+}
---
+++ PRN_SE1629_CONSUME_MATHLIB/Program.cs
@@ -0,0 +1,17 @@
+﻿using static MathLib.MathLib;
+namespace Prn.Math.MathLib;
+
+
+public class Program
+{
+    public static void Main()
+    {
+        int x = 10, y = 20;
+        int result;
+
+        result = x.Add(y);
+        result.Display();
+        Console.WriteLine(result);
+
+    }
+}
