3fd40d6bee9ed08d3b290abc759a137a4474e40a
datuwu <doixuihen@gmail.com>
2022-06-07 10:44:35 +0700

    learn filestream

---
+++ PRN_SE1629_FILESTREAM/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Prn.FileStreamDemo;
+
+public class Program
+{
+    public static void Main()
+    {
+        string msg = "Dang dau het ca dau day nay";
+
+        using FileStream fileStream = File.Open("data.dat",
+            FileMode.Create);
+        byte[] msgByte
+            = Encoding.UTF8.GetBytes(msg);
+
+        //write msgByte to file data.dat
+        fileStream.Write(msgByte, 0, msgByte.Length);
+        fileStream.Position = 0;
+    }
+}
